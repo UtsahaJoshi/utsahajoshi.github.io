f7d02161e6e4b9eb08bd2565927bc3673b404c25
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-05-08 19:19:10 +0545

    password-generator added

---
+++ password-generator/js/index.js
@@ -0,0 +1,118 @@
+var passwordLength = 12;
+var hasCharacter = {
+    lowercase: true,
+    uppercase: false,
+    numbers: false,
+    symbols: false
+}
+
+var lengthRangeInput = document.getElementById("option-length-range");
+var lengthInput = document.getElementById("option-length");
+
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var symbolicCharacters = "!@#$%^&*()-_+={}[].,<>:;";
+
+function passwordLengthValueChange(lengthValue) {
+    passwordLength = lengthValue;
+    lengthRangeInput.value = passwordLength;
+    lengthInput.value = passwordLength;
+}
+
+function toggleValueChange(whichToggle) {
+    switch(whichToggle){
+        case "lowercase":
+            hasCharacter.lowercase = !hasCharacter.lowercase;
+            break;
+        case "uppercase":
+            hasCharacter.uppercase = !hasCharacter.uppercase;
+            break;
+        case "number":
+            hasCharacter.numbers = !hasCharacter.numbers;
+            break;
+        case "symbol":
+            hasCharacter.symbols = !hasCharacter.symbols;
+            break;
+    }
+}
+
+function generatePassword() {
+    var password = "";
+    var mustHaveCharacterTypes = getMustHaveCharacterTypesNames();
+
+    for(var i = 0; i < passwordLength; i++){
+        
+        password += getACharacter(mustHaveCharacterTypes);
+    }
+    displayPasswordToUsers(password);
+}
+
+function displayPasswordToUsers(password){
+    var displayBoard = document.getElementById("password-display");
+    var copyButton = document.getElementById("copy-button");
+    var passwordText = document.getElementById("password-text");
+    passwordText.innerText = password;
+    displayBoard.style.display = "block";
+    copyButton.style.display = "block";
+}
+
+function getMustHaveCharacterTypesNames(){
+    var hasCharactersConfirmed = [];
+    Object.entries(hasCharacter).forEach(entry => {
+        const [key, value] = entry;
+        if (value) {
+            hasCharactersConfirmed.push(key);
+        }
+      });
+      return hasCharactersConfirmed;
+}
+function getACharacter(mustHaveCharacterTypes){
+    if (mustHaveCharacterTypes.length === 0){
+        return getRandomLetter("lowercase");
+    }
+    randomCharacterTypeIndex = Math.floor(Math.random() * mustHaveCharacterTypes.length);
+    randomCharacterType = mustHaveCharacterTypes[randomCharacterTypeIndex];
+    switch(randomCharacterType){
+        case "lowercase":
+            return getRandomLetter("lowercase");
+        case "uppercase":
+            return getRandomLetter("uppercase");
+        case "numbers":
+            return getRandomNumber();
+        case "symbols":
+            return getRandomSymbol();
+        default:
+            break;
+    }
+}
+function getRandomLetter(type){
+    var letter;
+    var randomStringIndex = Math.floor(Math.random() * lowerCaseCharacters.length);
+    letter = lowerCaseCharacters[randomStringIndex];
+    if (type === "lowercase"){
+        return letter;
+    }
+    return letter.toUpperCase();
+}
+
+function getRandomSymbol(){
+    var symbol;
+    var randomStringIndex = Math.floor(Math.random() * symbolicCharacters.length);
+    symbol = symbolicCharacters[randomStringIndex];
+    return symbol;
+}
+
+function getRandomNumber(){
+    var number;
+    number = Math.floor(Math.random()*10);
+    return number;
+}
+
+function copyPassword(){
+    var from = document.getElementById('password-text');
+    var range = document.createRange();
+    window.getSelection().removeAllRanges();
+    range.selectNode(from);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}

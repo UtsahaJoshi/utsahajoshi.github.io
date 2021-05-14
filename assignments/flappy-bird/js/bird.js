73b65f310197271c58b165bbc0c354c90cffa882
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-05-14 11:36:19 +0545

    gravity add

--- assignments/flappy-bird/js/bird.js
+++ assignments/flappy-bird/js/bird.js
@@ -1,4 +1,4 @@
-function Bird(speed){
+function Bird(speed) {
     this.speed = speed;
     this.sizeX = 50;
     this.sizeY = 40;
@@ -7,40 +7,40 @@ function Bird(speed){
     this.gravity = 2;
     this.birdAnimationState = [false, true, false];
     this.hoverState = true;
-    this.positionX = canvas.width/2.5;
-    this.positionY = canvas.height/2.5;
-    var hoverTopY = canvas.height/2.5 - 7;
-    var hoverBottomY = canvas.height/2.5 + 7;
+    this.positionX = canvas.width / 2.5;
+    this.positionY = canvas.height / 2.5;
+    var hoverTopY = canvas.height / 2.5 - 7;
+    var hoverBottomY = canvas.height / 2.5 + 7;
     var hoverUp = true;
     var targetPositionY = this.positionY;
     var jump = false;
     var jumpAcceleration = 2;
     this.gameOver = false;
-    var currentFrameData = {c: 1, r: 0, h: 64, w: 92}
+    var currentFrameData = { c: 1, r: 0, h: 64, w: 92 }
     var timeElapse = 0;
     var angle = 0;
 
     // on click jump
-    this.birdMovement = function() {  
+    this.birdMovement = function () {
         targetPositionY = this.positionY - jumpHeight;
         jump = true;
         jumpAcceleration += 2
-        angle = -45*Math.PI/180;
+        angle = -45 * Math.PI / 180;
     }
 
     //draw bird every frame
-    this.drawBird = function(){
+    this.drawBird = function () {
         timeElapse++;
         var bird = document.getElementById("bird");
         var canvasBird = document.createElement("canvas");
         var ctx2 = canvasBird.getContext("2d");
-        canvasBird.height = 2*this.sizeY;
-        canvasBird.width = 2*this.sizeX;
+        canvasBird.height = 2 * this.sizeY;
+        canvasBird.width = 2 * this.sizeX;
         canvas.appendChild(canvasBird);
 
         var animState = this.birdAnimationState.indexOf(true);
         var cropPosX;
-        switch(animState){
+        switch (animState) {
             case 0:
                 cropPosX = 0;
                 break;
@@ -51,22 +51,22 @@ function Bird(speed){
                 cropPosX = 185;
                 break;
         }
-        
+
         getPositionY();
 
         //get sprite
-        (timeElapse> 2 && !this.hoverState) ? currentFrameData = getCurrentFrame(bird) : 0;
+        (timeElapse > 2 && !this.hoverState) ? currentFrameData = getCurrentFrame(bird) : 0;
         ctx2.clearRect(0, 0, canvasBird.width, canvasBird.height);
-        var translateX= (canvasBird.width/2);
-        var translateY= (canvasBird.height/2);
-        ctx2.translate(translateX,translateY);
+        var translateX = (canvasBird.width / 2);
+        var translateY = (canvasBird.height / 2);
+        ctx2.translate(translateX, translateY);
         ctx2.rotate(angle); /* rotate on click */
         ctx2.drawImage(bird, currentFrameData.c * currentFrameData.w, currentFrameData.r * currentFrameData.h, currentFrameData.w, currentFrameData.h, 0, 0, this.sizeX, this.sizeY);
-        ctx.drawImage(canvasBird, this.positionX - this.sizeX, this.positionY, 2*this.sizeX, 2*this.sizeY)
+        ctx.drawImage(canvasBird, this.positionX - this.sizeX, this.positionY, 2 * this.sizeX, 2 * this.sizeY)
     }.bind(this);
 
     //get current frame from spritesheet
-    var getCurrentFrame = function(sprite) {
+    var getCurrentFrame = function (sprite) {
         timeElapse = 0;
         let numColumns = 3;
         let numRows = 1;
@@ -77,30 +77,30 @@ function Bird(speed){
         currentFrame++;
 
         let maxFrame = numColumns * numRows - 1;
-        if (currentFrame > maxFrame){
+        if (currentFrame > maxFrame) {
             currentFrame = 0;
         }
 
         let column = currentFrame % numColumns;
         let row = Math.floor(currentFrame / numColumns);
 
-        return {c: column, r: row, h: frameHeight, w: frameWidth}
+        return { c: column, r: row, h: frameHeight, w: frameWidth }
     }
 
     //update Y position
-    var getPositionY = function(){
-        if(this.hoverState && hoverUp && this.positionY < hoverTopY) {
+    var getPositionY = function () {
+        if (this.hoverState && hoverUp && this.positionY < hoverTopY) {
             hoverUp = false;
         }
 
-        if(this.hoverState && !hoverUp && this.positionY > hoverBottomY) {
+        if (this.hoverState && !hoverUp && this.positionY > hoverBottomY) {
             hoverUp = true;
         }
 
         if (jump && targetPositionY < this.positionY) {
             this.positionY -= jumpAcceleration;
             this.gravity = 0;
-        } else if(jump && targetPositionY >= this.positionY) {
+        } else if (jump && targetPositionY >= this.positionY) {
             targetPositionY = this.positionY;
             jump = false;
             angle = 0;
@@ -108,32 +108,32 @@ function Bird(speed){
             this.gravity = 2;
         }
 
-        if (!jump && this.gravity<3) {
+        if (!jump && this.gravity < 4) {
             this.gravity += 0.1;
         }
         if (!this.hoverState) {
             this.positionY += this.gravity;
         }
 
-        if (this.positionY > canvas.height - 180){
+        if (this.positionY > canvas.height - 180) {
             this.positionY = canvas.height - 180;
             this.gameOver = true;
         }
 
-        if (this.positionY < 0){
+        if (this.positionY < 0) {
             targetPositionY = 0;
             this.positionY = 0;
         }
 
-        if(this.hoverState && hoverUp) {
+        if (this.hoverState && hoverUp) {
             this.positionY -= 0.8;
-        } else if(this.hoverState && !hoverUp) {
+        } else if (this.hoverState && !hoverUp) {
             this.positionY += 0.8;
         }
     }.bind(this);
 
     //reset player variables
-    this.resetPlayer = function(){
+    this.resetPlayer = function () {
         hoverUp = true;
         targetPositionY = this.positionY;
         jump = false;
@@ -142,25 +142,24 @@ function Bird(speed){
         this.gravity = 2;
         this.birdAnimationState = [false, true, false];
         this.hoverState = true;
-        this.positionX = canvas.width/2.5;
-        this.positionY = canvas.height/2.5;
+        this.positionX = canvas.width / 2.5;
+        this.positionY = canvas.height / 2.5;
         this.gameOver = false;
     }
 
     //collision with pipes
-    this.collisionDetection = function(){
+    this.collisionDetection = function () {
         var posXCollide;
         var posYCollide;
-        for(var i=0; i<4; i++){
-            if (i%2 == 0){
+        for (var i = 0; i < 4; i++) {
+            if (i % 2 == 0) {
                 posXCollide = this.positionX > pipes[i].positionX - 40 && this.positionX < pipes[i].positionX + 100;
-                posYCollide = this.positionY>canvas.height - 150 - pipes[i].sizeY - 40 && this.positionY<canvas.height - 120;
+                posYCollide = this.positionY > canvas.height - 150 - pipes[i].sizeY - 40 && this.positionY < canvas.height - 120;
             } else {
                 var posXCollide = this.positionX > pipes[i].positionX - 40 && this.positionX < pipes[i].positionX + 100;
-                var posYCollide = this.positionY>0 && this.positionY<pipes[i].sizeY;
+                var posYCollide = this.positionY > 0 && this.positionY < pipes[i].sizeY;
             }
-            if (posXCollide && posYCollide)
-            {
+            if (posXCollide && posYCollide) {
                 this.gameOver = true;
             }
         }
--- assignments/flappy-bird/js/index.js
+++ assignments/flappy-bird/js/index.js
@@ -16,12 +16,12 @@ var pipes = [];
 var score = 0;
 var highScore = localStorage.getItem('highscore') || 0;
 
-for(var i=0; i<2; i++){
-    var size1 = Math.floor(Math.random() * (canvas.height/2.5)) + 50;
+for (var i = 0; i < 2; i++) {
+    var size1 = Math.floor(Math.random() * (canvas.height / 2.5)) + 50;
     var gap = Math.floor(Math.random() * 100) + 250;
     var size2 = Math.abs(640 - size1 - gap);
     var index1 = 2 * i;
-    var index2 = 2 * i + 1; 
+    var index2 = 2 * i + 1;
     pipes[index1] = new Pipe(index1, size1);
     pipes[index2] = new Pipe(index2, size2);
 }
@@ -29,19 +29,19 @@ for(var i=0; i<2; i++){
 var mouseClickPos = [];
 
 // draws canvas and maintains the game loop
-function drawCanvas(){
+function drawCanvas() {
     if (!playerObj.gameOver) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height- 100);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height - 100);
         drawAnimatingGround();
-        if(!playerObj.hoverState){
+        if (!playerObj.hoverState) {
             drawPipes();
             playerObj.collisionDetection();
             scoreUpdate();
             ctx.font = "50px Flappy";
             ctx.fillStyle = "white";
-            ctx.fillText(score, canvas.width/2, 160);
+            ctx.fillText(score, canvas.width / 2, 160);
         }
         playerObj.drawBird();
     } else {
@@ -54,20 +54,20 @@ function drawCanvas(){
 
 //update the score
 function scoreUpdate() {
-    for(var i=0; i<4; i++){
-        if (playerObj.positionX>pipes[i].positionX && !pipes[i].scored) {
+    for (var i = 0; i < 4; i++) {
+        if (playerObj.positionX > pipes[i].positionX && !pipes[i].scored) {
             score = score + 0.5; //since there are two pipe objects
             pipes[i].scored = true;
         }
     }
-    (score>highScore) ? highScore = score : 1;
+    (score > highScore) ? highScore = score : 1;
 }
 
 //draw the pipes
-function drawPipes(){
-    for(var i=0; i<2; i++){
+function drawPipes() {
+    for (var i = 0; i < 2; i++) {
         var index1 = 2 * i;
-        var index2 = 2 * i + 1; 
+        var index2 = 2 * i + 1;
         pipes[index1].drawPipe()
         pipes[index2].drawPipe()
     }
@@ -75,29 +75,29 @@ function drawPipes(){
 
 //game over menu
 function gameOverMenu() {
-    var x = canvas.width/2 - 100;
-    var y = canvas.height/2 - 200;
+    var x = canvas.width / 2 - 100;
+    var y = canvas.height / 2 - 200;
 
     ctx.drawImage(scoreBoard, x, y, 200, 200);
     ctx.font = "30px Flappy";
     ctx.fillStyle = "white";
-    ctx.fillText(score, canvas.width/2 - 10, 210);
-    ctx.fillText(highScore, canvas.width/2 - 15, 285);
+    ctx.fillText(score, canvas.width / 2 - 10, 210);
+    ctx.fillText(highScore, canvas.width / 2 - 15, 285);
     ctx.drawImage(restart, x, y + 220, 200, 50);
 
 
-    if(mouseClickPos[0]>= x && mouseClickPos[0] <= x + 200 && mouseClickPos[1] >= y + 220 && mouseClickPos[1] <= y + 270) {
+    if (mouseClickPos[0] >= x && mouseClickPos[0] <= x + 200 && mouseClickPos[1] >= y + 220 && mouseClickPos[1] <= y + 270) {
         playerObj.resetPlayer();
         this.score = 0;
-        for(var i=0; i<4; i++){
+        for (var i = 0; i < 4; i++) {
             pipes[i].scored = false;
             if (i % 2 === 0) {
                 pipes[i].positionX = canvas.width + 150 * i;
-                pipes[i].sizeY = Math.floor(Math.random() * (canvas.height/2.5)) + 50;
+                pipes[i].sizeY = Math.floor(Math.random() * (canvas.height / 2.5)) + 50;
                 var gap = Math.floor(Math.random() * 100) + 250;
-                pipes[i+1].sizeY = Math.abs(640 - pipes[i].sizeY - gap);
+                pipes[i + 1].sizeY = Math.abs(640 - pipes[i].sizeY - gap);
             } else {
-                pipes[i].positionX = canvas.width + 150 * (i-1);
+                pipes[i].positionX = canvas.width + 150 * (i - 1);
             }
         }
     }
@@ -105,22 +105,22 @@ function gameOverMenu() {
 }
 
 //animating ground
-function drawAnimatingGround(){
-    for(var i = 0; i< canvas.width/30 + 1; i++){
-        (!groundPosX[i] && groundPosX[i] !== 0) ? groundPosX[i] = (i*30) : groundPosX[i] -= playerObj.speed;
-        if (groundPosX[i]<-30) {
-            groundPosX[i]=groundPosX[i]+canvas.width+30;
+function drawAnimatingGround() {
+    for (var i = 0; i < canvas.width / 30 + 1; i++) {
+        (!groundPosX[i] && groundPosX[i] !== 0) ? groundPosX[i] = (i * 30) : groundPosX[i] -= playerObj.speed;
+        if (groundPosX[i] < -30) {
+            groundPosX[i] = groundPosX[i] + canvas.width + 30;
         }
-        ctx.drawImage(ground, groundPosX[i], canvas.height-100, 30, 100);
+        ctx.drawImage(ground, groundPosX[i], canvas.height - 100, 30, 100);
     }
 }
 
 requestAnimationFrame(drawCanvas);
 
 // click listener for user input
-canvas.addEventListener('click', function(e) {
-    var rect = canvas.getBoundingClientRect(); 
-    var x = e.clientX - rect.left; 
+canvas.addEventListener('click', function (e) {
+    var rect = canvas.getBoundingClientRect();
+    var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     mouseClickPos = [x, y];
 
